39b6c4145ddbc8ecf4368a52ddf600e57d7f61c4
PCFIXETHEO <theo.lebegue@etu.uphf.fr>
2023-01-24 21:32:08 +0100

    add new sources and maven config

---
+++ src/main/java/com/projetapi/lebegue/projetapi/ProjetapiApplication.java
@@ -0,0 +1,13 @@
+package com.projetapi.lebegue.projetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetapiApplication.class, args);
+	}
+
+}

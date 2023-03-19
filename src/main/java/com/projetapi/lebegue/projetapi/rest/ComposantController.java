479acdc3b2ed94fcc3b73efec1c5ea15fa18dfd2
ugo-bertrand <ugo.bertrand@epitech.eu>
2023-03-19 12:16:59 +0100

    add CRUD methods in composant controller and service

--- src/main/java/com/projetapi/lebegue/projetapi/rest/ComposantController.java
+++ src/main/java/com/projetapi/lebegue/projetapi/rest/ComposantController.java
@@ -3,9 +3,7 @@ package com.projetapi.lebegue.projetapi.rest;
 import com.projetapi.lebegue.projetapi.model.Composant;
 import com.projetapi.lebegue.projetapi.services.ComposantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -17,4 +15,25 @@ public class ComposantController {
     public ArrayList<Composant> findAll(){
         return composantService.findAll();
     }
+
+    @GetMapping("/composant/{id}")
+    public Composant findById(@PathVariable(name = "id") String id){
+        return composantService.findById(id);
+    }
+
+    @PostMapping("/add")
+    public void createComposant(@RequestBody Composant composant){
+        composantService.createComposant(composant);
+    }
+
+    @PutMapping("/update/{id}/composant")
+    public void updateComposant(@RequestBody Composant composant, @PathVariable(name = "id", required = true) String id){
+        composantService.updateComposantById(id,composant);
+    }
+
+    @DeleteMapping("/delete/{id}/composant")
+    public void deleteComposant(@PathVariable(name = "id") String id){
+        composantService.deleteComposantById(id);
+    }
+
 }
